f842b2cf420affc2dc6b2872a53c1cae4ba6e7b8
antoinelyset <antoinelyset+github@gmail.com>
2023-10-25 18:16:36 +0200

    Update conf for teknium/OpenHermes-2-Mistral-7B

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
-# mistralai/Mistral-7B-v0.1 Cog model
+# teknium/OpenHermes-2-Mistral-7B Cog
 
-This is an implementation of the [mistralai/Mistral-7B-v0.1](https://huggingface.co/mistralai/Mistral-7B-v0.1) as a Cog model. [Cog packages machine learning models as standard containers.](https://github.com/replicate/cog)
+## About
 
-First, download the pre-trained weights:
+This is a very basic implementation of [teknium/OpenHermes-2-Mistral-7B](https://huggingface.co/teknium/OpenHermes-2-Mistral-7B) as a Cog model.
+
+- OpenAI `messages` API supported as JSON `prompt`, thanks to `tokenizer.apply_chat_template` so it should work with any `tokenizer_config.json` with a `"chat_template"`, [example](https://huggingface.co/teknium/OpenHermes-2-Mistral-7B/blob/main/tokenizer_config.json#L52).
+- Tensorizer to convert the pre-downloaded weights, and speed the boot time. You will need quite a bit of RAM (around 2 times the model size, so here ~26GB). It's also possible to download them at runtime, you will need to update `predict.py`.
+- Simple inference engine with `transformers`
+- You can swap the `MODEL_NAMEL` and change the `name` in `cog.yaml` and it should work with a lot of different Hugging Face models.
+
+## Usage
+
+You will need to install [Cog](https://github.com/replicate/cog/blob/main/docs/getting-started.md#install-cog)
+
+Then download the pre-trained weights:
 
     cog run script/download-weights
 
-Then, you can run predictions:
+Then build the container:
 
-    og predict -i prompt="Explain in a short paragraph quantu field theory to a high school student"
+    cog build --separate-weights
+
+Finally, run the container and test it:
+
+    docker run --gpus all -p 9090:8080 r8.im/antoinelyset/openhermes-2-mistral-7b
+    curl -X POST -H "Content-Type: application/json" -d '{"input": {"prompt": "[{\"role\": \"system\",\"content\":\"You are a helpful assistant.\"},{\"role\": \"user\",\"content\": \"What is Slite?\"}]"}}' http://localhost:9090/predictions
+
+You can push it to Replicate, first create a model [https://replicate.com/create](https://replicate.com/create) and then:
+
+    cog login
+    docker push r8.im/antoinelyset/openhermes-2-mistral-7b
+
+You can now test it on their servers [https://replicate.com/antoinelyset/openhermes-2-mistral-7b](https://replicate.com/antoinelyset/openhermes-2-mistral-7b)
--- predict.py
+++ predict.py
@@ -1,45 +1,82 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
-from cog import BasePredictor, Input
+import os
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import json
+from cog import BasePredictor, Input, ConcatenateIterator
+from tensorizer import TensorDeserializer
+from tensorizer.utils import no_init_or_tensor
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, TextIteratorStreamer
+from threading import Thread
 
-MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.1"
-MODEL_CACHE = "model-cache"
-TOKEN_CACHE = "token-cache"
+
+MODEL_NAME = "teknium/OpenHermes-2-Mistral-7B"
+WEIGHTS_CACHE = "weights"
+TENSORIZED_MODEL_NAME = f"{MODEL_NAME.split('/')[-1]}.tensors"
+TENSORIZED_MODEL_PATH = os.path.join(WEIGHTS_CACHE, TENSORIZED_MODEL_NAME)
+
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
         self.tokenizer = AutoTokenizer.from_pretrained(
             MODEL_NAME,
-            cache_dir=TOKEN_CACHE
+            cache_dir=WEIGHTS_CACHE
         )
-        self.model = AutoModelForCausalLM.from_pretrained(
-            MODEL_NAME,
-            torch_dtype=torch.float16,
-            cache_dir=MODEL_CACHE
-        ).to('cuda')
-
+        config = AutoConfig.from_pretrained(
+            MODEL_NAME, cache_dir=WEIGHTS_CACHE)
+        with no_init_or_tensor():
+            self.model = AutoModelForCausalLM.from_config(config)
+        deserializer = TensorDeserializer(os.path.join(
+            WEIGHTS_CACHE, TENSORIZED_MODEL_NAME), plaid_mode=True)
+        deserializer.load_into_module(self.model)
+        deserializer.close()
+        self.model.eval()
 
     def predict(
         self,
-        prompt: str = Input(description="Input prompt"),
-        max_new_tokens: int = Input(description="Max new tokens", ge=0, le=2048, default=512),
-    ) -> str:
+        prompt: str = Input(
+            description="The JSON stringified of the messages (array of objects with role/content like OpenAI) to predict on"),
+        max_new_tokens: int = Input(
+            description="Max new tokens", ge=1, default=512),
+        temperature: float = Input(
+            description="Adjusts randomness of outputs, greater than 1 is random and 0 is deterministic, 0.75 is a good starting value.",
+            ge=0.01,
+            le=5,
+            default=0.75,
+        ),
+        top_p: float = Input(
+            description="When decoding text, samples from the top p percentage of most likely tokens; lower to ignore less likely tokens",
+            ge=0.0,
+            le=1.0,
+            default=0.9,
+        ),
+        top_k: int = Input(
+            description="When decoding text, samples from the top k most likely tokens; lower to ignore less likely tokens",
+            ge=0,
+            default=50,
+        ),
+    ) -> ConcatenateIterator:
         """Run a single prediction on the model"""
-        encodeds = self.tokenizer(
-            prompt,
-            return_tensors="pt",
-            add_special_tokens=False
-        )
-        model_inputs = encodeds.to('cuda')
-        generated_ids = self.model.generate(
-            **model_inputs,
+        messages = self.tokenizer.apply_chat_template(
+            json.loads(prompt), tokenize=False, add_generation_prompt=True)
+        tokens_in = self.tokenizer(
+            messages,
+            return_tensors="pt"
+        ).input_ids.to('cuda')
+        streamer = TextIteratorStreamer(
+            self.tokenizer, timeout=600.0, skip_prompt=True, skip_special_tokens=True)
+        generate_kwargs = dict(
+            input_ids=tokens_in,
+            streamer=streamer,
+            do_sample=True,
             max_new_tokens=max_new_tokens,
-            do_sample=True
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
         )
-        decoded = self.tokenizer.batch_decode(generated_ids)
-        result = decoded[0]
-        return result
+        t = Thread(target=self.model.generate, kwargs=generate_kwargs)
+        t.start()
+        for out in streamer:
+            yield out
